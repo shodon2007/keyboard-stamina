d5e1a7a2311b272d4bca20548f9ad643c401759c
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-11-12 09:43:05 +0700

    added function in finish

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 let text = "lorem ipsum is a new version in a last on week to search youtube";
 let letter = text.split('');
+let mistakes = 0;
 let thisLetter = 0;
 let seeText = document.querySelector('.text');
-let seeButtons = document.querySelector('.buttons');
+// let seeButtons = document.querySelector('.buttons');
 seeText.innerHTML = text.slice(thisLetter);
 
-document.addEventListener('keydown', (button) => {
+document.addEventListener('keyup', (button) => {
     if (button.key == letter[thisLetter]) {
         thisLetter++;
         seeText.innerHTML = text.slice(thisLetter);
-        seeButtons.innerHTML = button.key;
     } else {
-        console.log("Неправильно")
+        mistakes++;
+        console.log("Количество ошибок: " + mistakes)
+    }
+
+    if (text.slice(thisLetter) == false) {
+        console.log('finish')
     }
 })
