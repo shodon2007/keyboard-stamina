a32aff241f81df5718a21e141b9113ed697bd230
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-11-14 06:54:22 +0700

    added functional buttons and active buttons

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
-let text = "lorem ipsum is a new version in a last on week to search youtube";
+let text = "21312321313112154656";
 let letter = text.split('');
 let mistakes = 0;
 let thisLetter = 0;
 let seeText = document.querySelector('.text');
-// let seeButtons = document.querySelector('.buttons');
+let seeButtons = document.querySelector('.buttons');
 seeText.innerHTML = text.slice(thisLetter);
 
 document.addEventListener('keyup', (button) => {
+    resetStyles();
+    let seeButton = document.getElementById(button.key);
+
     if (button.key == letter[thisLetter]) {
+        seeButton.classList.add('active');
         thisLetter++;
         seeText.innerHTML = text.slice(thisLetter);
     } else {
         mistakes++;
-        console.log("Количество ошибок: " + mistakes)
+        console.log("Количество ошибок: " + mistakes);
     }
 
     if (text.slice(thisLetter) == false) {
         console.log('finish')
     }
-})
+});
+
+function resetStyles() {
+    let buttonList = document.querySelectorAll('.button')
+    buttonList.forEach((key) => {
+        key.classList.remove('active');
+    })
+}
