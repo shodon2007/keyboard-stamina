71c048b89ea5b67348e9a2021bf7844c4f540724
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-11-20 14:01:34 +0700

    finished keyboard

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let text = "21312321313112154656";
+let text = "hellomynameisshodonasrorovandiam15yearsold";
 let letter = text.split('');
 let mistakes = 0;
 let thisLetter = 0;
@@ -6,27 +6,44 @@ let seeText = document.querySelector('.text');
 let seeButtons = document.querySelector('.buttons');
 seeText.innerHTML = text.slice(thisLetter);
 
+print();
+
 document.addEventListener('keyup', (button) => {
+    console.log(button.key);
     resetStyles();
+    print();
     let seeButton = document.getElementById(button.key);
+    seeButton.classList.add('click');
 
     if (button.key == letter[thisLetter]) {
-        seeButton.classList.add('active');
         thisLetter++;
-        seeText.innerHTML = text.slice(thisLetter);
+        print();
+
+        if (text.slice(thisLetter) == false) {
+            console.log('finish');
+        }
     } else {
         mistakes++;
-        console.log("Количество ошибок: " + mistakes);
     }
 
-    if (text.slice(thisLetter) == false) {
-        console.log('finish')
-    }
+    console.log();
+    document.getElementById(text[thisLetter])?.classList.add("active");
 });
 
 function resetStyles() {
-    let buttonList = document.querySelectorAll('.button')
+    let buttonList = document.querySelectorAll('.button');
     buttonList.forEach((key) => {
         key.classList.remove('active');
+        key.classList.remove('click');
     })
+}
+
+function print() {
+    let result = text.slice(thisLetter);
+    let printed = text.slice((thisLetter >= 10) ? (thisLetter - 10) : 0, thisLetter);
+    if (result.length >= 20) {
+        result = result.slice(0, 20);
+        result += "...";
+    }
+    seeText.innerHTML = `<div class="printed">${printed}</div>` + result;
 }
